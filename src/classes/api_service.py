52311b498a9bd1221ed4f2d493204ff0e90f8746
radheem <radheem98@gmail.com>
2022-10-05 19:56:00 +0500

    add comments to api_service

--- src/classes/api_service.py
+++ src/classes/api_service.py
@@ -1,14 +1,20 @@
 import requests
 from interfaces import *
-from constants import SERVICE_URLS
 
 class APIService():
     def __init__(self, url):
-        self.server_url = url.removesuffix('/')
+        self.server_url = str(url).removesuffix('/')
         self.auth_token = None
         return
     
     def get(self, service_url, query, auth_required=False):
+        """
+            Makes a GET request and returns the results
+            Inputs:
+                - service_url(str): the url to make the request to.
+                - query(dict): the get query.
+                - auth_required(bool): indicates whether authorization is required for the call or not.  
+        """
         url = self._create_url(service_url)
         if auth_required:
             return requests.get(url=url, params=query, headers={'Authorization': 'Bearer {}'.format(self.auth_token)}).json()
@@ -16,6 +22,13 @@ class APIService():
             return requests.get(url, params=query).json()
         
     def post(self, service_url, data, auth_required=False):
+        """
+            Makes a POST request and returns the results
+            Inputs:
+                - service_url(str): the url to make the request to.
+                - data(dict): the data to post with POST request.
+                - auth_required(bool): indicates whether authorization is required for the call or not.
+        """
         url = self._create_url(service_url)
         if auth_required:
             return requests.post(url=url, data=data, headers={'Authorization': 'Bearer {}'.format(self.auth_token)}).json()
@@ -23,6 +36,13 @@ class APIService():
             return requests.post(url=url, data=data).json()
 
     def delete(self,service_url, data, auth_required=False):
+        """
+            Makes a DELETE request and returns the results
+            Inputs:
+                - service_url(str): the url to make the request to.
+                - data(dict): the data to post with POST request.
+                - auth_required(bool): indicates whether authorization is required for the call or not.
+        """
         url = self._create_url(service_url)
         if auth_required:
             return requests.delete(url=url, data=data, headers={'Authorization': 'Bearer {}'.format(self.auth_token)}).json()
@@ -33,4 +53,7 @@ class APIService():
         Private methods
     '''
     def _create_url(self, path):
+        """
+            appends namespace to server url
+        """
         return "{}{}".format(self.server_url, path)
