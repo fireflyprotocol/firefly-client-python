3c169366872b090b9c4a1f40245c546f83a0e28f
YameenMalik <yameen@seed.im>
2022-12-19 13:36:22 +0500

    example for order cancellation

--- examples/7.cancelling_orders.py
+++ examples/7.cancelling_orders.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 # paths
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -10,8 +11,7 @@ sys.path.append(os.path.abspath(os.path.join(script_dir, "../src/classes")))
 from config import TEST_ACCT_KEY
 from firefly_client import FireflyClient
 from constants import Networks
-from interfaces import OrderCancellationRequest
-from enumerations import MARKET_SYMBOLS, ORDER_STATUS
+from enumerations import MARKET_SYMBOLS, ORDER_STATUS, ORDER_SIDE, ORDER_TYPE
 from pprint import pprint
 
 
@@ -28,19 +28,39 @@ def main():
     # must add market before cancelling its orders
     client.add_market(MARKET_SYMBOLS.ETH)
 
-    # get user open orders
-    open_orders = client.get_orders({
-        "symbol": MARKET_SYMBOLS.ETH, 
-        "statuses": [ORDER_STATUS.OPEN]
-        })
 
+
+    order = {
+        "symbol":MARKET_SYMBOLS.ETH, 
+        "price":1301, 
+        "quantity":0.1, 
+        "side":ORDER_SIDE.SELL, 
+        "orderType":ORDER_TYPE.LIMIT,
+        "leverage":client.get_user_leverage(MARKET_SYMBOLS.ETH),
+        "expiration":int(time.time()+(30*24*60*60)), # a random time in future
+        "reduceOnly":False,
+        "salt":10,
+        "postOnly":True
+    }
+
+
+    signed_order = client.create_signed_order(order);
+    resp = client.post_signed_order(signed_order)
     
-    # sign order for cancellation using hash
-    cancellation_request = client.create_signed_cancel_orders(MARKET_SYMBOLS.ETH, order_hash=[open_orders[0]['hash']])
+    
+    # sign order for cancellation using order hash
+    # you can pass a list of hashes to be signed for cancellation, good to be used when multiple orders are to be cancelled
+    cancellation_request = client.create_signed_cancel_orders(MARKET_SYMBOLS.ETH, order_hash=[resp['hash']])
+    pprint(cancellation_request)
 
+    # or sign the order for cancellation using order data
+    cancellation_request = client.create_signed_cancel_order(order)
+    pprint(cancellation_request) # same as above cancellation request
+
+    # post order to exchange for cancellation
     resp = client.post_cancel_order(cancellation_request)
     
-    print(resp)
+    pprint(resp)
 
 if __name__ == "__main__":
     main()
