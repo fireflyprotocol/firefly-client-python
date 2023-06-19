447368bb955e55fce23e27ab7eca22f8a31d67dd
Nida Karim Ali <nidakarimali@ip-192-168-0-133.ec2.internal>
2023-06-19 12:13:23 +0500

    added examples

---
+++ examples/19.Generate_readonly_token.py
@@ -0,0 +1,33 @@
+from config import TEST_ACCT_KEY, TEST_NETWORK
+from firefly_exchange_client import FireflyClient, Networks
+from pprint import pprint
+import asyncio
+
+
+
+async def main():
+  # initialize client
+  client = FireflyClient(
+      True, # agree to terms and conditions
+      Networks[TEST_NETWORK], # network to connect with
+      TEST_ACCT_KEY, # private key of wallet
+      )
+
+  # initialize the client
+  # on boards user on firefly. Must be set to true for first time use
+  await client.init(True,"") 
+  
+  print('Account Address:', client.get_public_address()) 
+
+  # # generates read-only token for user
+  data = await client.generate_readonly_token()
+
+  # close aio http connection
+  await client.apis.close_session()
+
+  pprint(data)
+
+if __name__ == "__main__":
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(main())
+  loop.close()
