4ba30b03e10e6346ae6f252f81517c9b416c17ef
Kulsoom Malik <kulsoom@seed.im>
2023-08-15 17:26:54 +0500

    Added growth service routes

--- src/firefly_exchange_client/constants.py
+++ src/firefly_exchange_client/constants.py
@@ -103,6 +103,24 @@ SERVICE_URLS = {
     "ORDERS": "/orders",
     "ORDERS_HASH": "/orders/hash",
   },
+  "GROWTH": {
+    "REFERRER_INFO": "/growth/getReferrerInfo",
+    "CAMPAIGN_DETAILS": "/growth/campaignDetails",
+    "CAMPAIGN_REWARDS": "/growth/campaignRewards",
+    "AFFILIATE_PAYOUTS": "/growth/affiliate/payouts",
+    "AFFILIATE_REFEREE_DETAILS": "/growth/affiliate/refereeDetails",
+    "AFFILIATE_REFEREES_COUNT": "/growth/affiliate/refereesCount",
+    "USER_REWARDS_HISTORY": "/growth/userRewards/history",
+    "USER_REWARDS_SUMMARY": "/growth/userRewards/summary",
+    "REWARDS_OVERVIEW": "/growth/tradeAndEarn/rewardsOverview",
+    "REWARDS_DETAILS": "/growth/tradeAndEarn/rewardsDetail",
+    "TOTAL_HISTORICAL_TRADING_REWARDS": "/growth/tradeAndEarn/totalHistoricalTradingRewards",
+    "MAKER_REWARDS_SUMMARY": "/growth/marketMaker/maker-rewards-summary",
+    "MAKER_REWARDS_DETAILS": "/growth/marketMaker/maker-rewards-detail",
+    "MAKER_WHITELIST_STATUS": "/growth/marketMaker/whitelist-status",
+    "GENERATE_CODE": "/growth/generateCode",
+    "LINK_REFERRED_USER": "/growth/linkReferredUser"
+  },
 }
 
 EIP712_CANCEL_ORDER_STRUCT_STRING ="CancelLimitOrder(string action,bytes32[] orderHashes)"
