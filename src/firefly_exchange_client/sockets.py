2c405b926ff75b12a50beba6ea3fb58273179a32
Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>
2023-10-22 14:28:33 +0500

    orderbook partial depth

--- src/firefly_exchange_client/sockets.py
+++ src/firefly_exchange_client/sockets.py
@@ -20,6 +20,7 @@ class Sockets:
             Connects to the desired url
         """
         try:
+            print(self.url)
             await sio.connect(self.url, wait_timeout=self.timeout,
                         transports=["websocket"])
             return True
@@ -135,6 +136,49 @@ class Sockets:
         except Exception as e:
             print(e)
             return False
+
+    async def subscribe_orderbook_depth_streams_by_symbol(self, symbol: MARKET_SYMBOLS, depth=""):
+        """
+            Allows user to subscribe to orderbook depth stream for the desired symbol.
+            Inputs:
+                - symbol: market symbol of market user wants orderbook depth stream for. (e.g. DOT-PERP)
+                - depth: depth of orderbook depth stream (optional)
+        """
+        try:
+            resp = await sio.call('SUBSCRIBE', [
+                {
+                    "e": SOCKET_EVENTS.ORDERBOOK_DEPTH_STREAM_ROOM.value,
+                    "p": symbol.value,
+                    "d": depth
+                    
+                },
+            ])
+            return resp["success"]
+        except Exception as e:
+            print("Error: ", e)
+            return False
+
+    async def unsubscribe_orderbook_depth_streams_by_symbol(self, symbol: MARKET_SYMBOLS, depth):
+        """
+            Allows user to unsubscribe to orderbook depth stream for the desired symbol.
+            Inputs:
+                - symbol: market symbol of market user wants orderbook depth stream for. (e.g. DOT-PERP)
+                - depth: depth of orderbook depth stream (optional)
+        """
+        try:
+            resp = await sio.call('UNSUBSCRIBE', [
+                {
+                    "e": SOCKET_EVENTS.ORDERBOOK_DEPTH_STREAM_ROOM.value,
+                    "p": symbol.value,
+                    "d": depth
+                    
+                },
+            ])
+            return resp["success"]
+        except Exception as e:
+            print("Error: ", e)
+            return False
+
 
     async def subscribe_user_update_by_token(self, parent_account: str = None, user_token: str = None) -> bool:
         """
